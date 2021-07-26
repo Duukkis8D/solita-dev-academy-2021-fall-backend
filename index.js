d170b63d0ca0e162cfa803fcea2ffedb49b80184
Tuukka Virtanen <duukkis@gmail.com>
2021-07-26 23:59:50 +0300

    server port number is coming from .env file

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.get( '/api/vaccinations', ( req, res ) => {
 	} )
 } )
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen( PORT, () => {
 	console.log( `Server running on port ${PORT}` )
 } )
