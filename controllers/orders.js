f7bcc479b67eeb16c2f4eb2d125b9447532da8c6
Tuukka Virtanen <duukkis@gmail.com>
2021-08-14 02:55:25 +0300

    two routes for the dates of first and latest vaccine orders

--- controllers/orders.js
+++ controllers/orders.js
@@ -1,6 +1,50 @@
 const ordersRouter = require( 'express' ).Router()
 const Order = require( '../models/order' )
 
+ordersRouter.get( '/firstOrderDate', ( req, res ) => {
+	Order
+		.aggregate(
+			[
+				{ $project: {
+					'arrived': 1,
+					'date': { $dateFromString: { 'dateString': '$arrived' } }
+				} },
+				{ $group: {
+					_id: null,
+					firstOrder: { $min: '$date' }
+				} },
+				{ $project: {
+					_id: 0,
+					firstOrder: 1
+				} }
+			] )
+		.then( result => {
+			res.json( result )
+		} )
+} )
+
+ordersRouter.get( '/latestOrderDate', ( req, res ) => {
+	Order
+		.aggregate(
+			[
+				{ $project: {
+					'arrived': 1,
+					'date': { $dateFromString: { 'dateString': '$arrived' } }
+				} },
+				{ $group: {
+					_id: null,
+					latestOrder: { $max: '$date' }
+				} },
+				{ $project: {
+					_id: 0,
+					latestOrder: 1
+				} }
+			] )
+		.then( result => {
+			res.json( result )
+		} )
+} )
+
 ordersRouter.get( '/amountOfVaccines/:dateAndTime', ( req, res ) => {
 	const dateAndTime = new Date( req.params.dateAndTime )
 
