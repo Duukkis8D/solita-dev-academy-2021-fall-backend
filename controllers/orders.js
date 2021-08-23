5c0f5f163a96a6861e5edd114fae33f86cd2f152
Tuukka Virtanen <duukkis@gmail.com>
2021-08-23 19:51:28 +0300

    route for vaccines expired before use

--- controllers/orders.js
+++ controllers/orders.js
@@ -100,4 +100,46 @@ ordersRouter.get( '/amountOfVaccines/:dateAndTime', ( req, res ) => {
 		} )
 } )
 
+ordersRouter.get( '/vaccinesExpiredBeforeUse/:dateAndTime', ( req, res ) => {
+	const dateAndTime = new Date( req.params.dateAndTime )
+
+	Order
+		.aggregate(
+			[
+				{ $lookup: {
+					'localField': 'id',
+					'from': 'vaccinations',
+					'foreignField': 'sourceBottle',
+					'as': 'vaccinations'
+				} },
+				{ $project: {
+					_id: 0,
+					injections: 1,
+					arrivedDate: { $dateFromString: { 'dateString': '$arrived' } },
+					usedVaccinesInBottle: { $size: '$vaccinations' }
+				} },
+				{ $project: {
+					arrivedDate: 1,
+					unUsedVaccinesInBottle: { $subtract: [ '$injections', '$usedVaccinesInBottle' ] }
+				} },
+				{ $project: {
+					expiredVaccinesInBottle: {
+						$cond: { if: { $lte: [ { $add: [ '$arrivedDate', 30*24*60*60000 ] }, dateAndTime ] }, then: '$unUsedVaccinesInBottle', else: 0 }
+					}
+				} },
+				{ $group: {
+					_id: null,
+					expiredVaccinesTotal: { $sum: '$expiredVaccinesInBottle' }
+				} },
+				{ $project: {
+					_id: 0,
+					expiredVaccinesTotal: 1
+				} }
+			] )
+		.then( result => {
+			if( result.length > 0 ) res.json( result )
+			else res.json( [ { expiredVaccinesTotal: 0 } ] )
+		} )
+} )
+
 module.exports = ordersRouter
